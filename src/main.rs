999ade84e523b85bb3f5551057a9d4cb59b4f423
Aayush Shah <aayush.shah15@gmail.com>
2020-05-03 17:45:31 -0400

    *: free standing rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+// Do not use the normal entry point chain (no C runtime)
+#![no_main]
+// Do not try to link the standard library
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+// Note: '!' indicates this is a diverging function (it never returns)
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// Note: the 'no_mangle' attribute tells the compiler to output the function
+// symbol without mangling with its name. We need this in order to tell the linker
+// about our new entry point. 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
